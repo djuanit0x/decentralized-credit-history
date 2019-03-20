57d4063d62ed8c4ccd4e9695955d84634d6605ce
dennisjuanito <dennisjuanito@gmail.com>
2019-03-20 16:07:21 +0700

    render a repayment tables for each loans

--- client/components/RepaymentsRow.js
+++ client/components/RepaymentsRow.js
@@ -6,63 +6,16 @@ import Loan from '../utils/loan';
 
 const {Row, Cell} = Table;
 
-export default ({
-    id,
-    description,
-    value,
-    recipient,
-    approvalCount,
-    approversCount,
-    address,
-    index,
-    complete
-}) => {
+export default ({month, balance, date, interest, monthToIsPaid, payment, principal}) => {
     return (
-        <Row disabled={complete} positive={approvalCount > approversCount / 2 && !complete}>
-            <Cell>{id}</Cell>
-            <Cell>{description}</Cell>
-            <Cell>{web3.utils.fromWei(value, 'ether')}</Cell>
-            <Cell>{recipient}</Cell>
-            <Cell>
-                {approvalCount}/{approversCount}
-            </Cell>
-            <Cell>
-                {complete ? null : (
-                    <Button
-                        color='green'
-                        basic
-                        onClick={async () => {
-                            const campaign = Campaign(address);
-                            console.log(campaign);
-                            const accounts = await web3.eth.getAccounts();
-                            console.log(accounts);
-                            await campaign.methods.approveRequest(id).send({
-                                from: accounts[0]
-                            });
-                        }}
-                    >
-                        Approve
-                    </Button>
-                )}
-            </Cell>
-            <Cell>
-                {complete ? null : (
-                    <Button
-                        color='teal'
-                        basic
-                        onClick={async () => {
-                            const campaign = Campaign(address);
-
-                            const accounts = await web3.eth.getAccounts();
-                            await campaign.methods.finalizeRequest(id).send({
-                                from: accounts[0]
-                            });
-                        }}
-                    >
-                        Finalize
-                    </Button>
-                )}
-            </Cell>
+        <Row>
+            <Cell>{month}</Cell>
+            <Cell>{date}</Cell>
+            <Cell>{balance}</Cell>
+            <Cell>{payment}</Cell>
+            <Cell>{interest}</Cell>
+            <Cell>{principal}</Cell>
+            <Cell>{monthToIsPaid ? 'true' : 'false'}</Cell>
         </Row>
     );
 };
--- client/pages/loans/show.js
+++ client/pages/loans/show.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
-import {Button, Table} from 'semantic-ui-react';
+import {Table, Card, Grid, CardContent} from 'semantic-ui-react';
+import Layout from '../../components/Layout';
+import RePaymentRow from '../../components/RepaymentsRow';
 import Loan from '../../utils/loan';
 
 export default class componentName extends Component {
     static async getInitialProps(props) {
-        const loan = await Loan(props.query.address);
+        const loanContractAddress = props.query.address;
+        const loan = await Loan(loanContractAddress);
         const rePaymentsCount = await loan.methods.getRepaymentsCount().call();
         const loanTitle = await loan.methods.loanTitle().call();
         let lenderAddress = await loan.methods.lender().call();
@@ -17,14 +20,41 @@ export default class componentName extends Component {
                 })
         );
 
-        return {rePayments, loanTitle, lenderAddress, borrowerAddress};
+        return {rePayments, loanTitle, lenderAddress, borrowerAddress, loanContractAddress};
     }
+
+    renderRows = () => {
+        return this.props.rePayments.map((rePayment, index) => {
+            return <RePaymentRow key={index} month={index} {...rePayment} />;
+        });
+    };
     render() {
-        console.log(this.props);
+        const {Header, Row, HeaderCell, Body} = Table;
         return (
-            <div>
-                <p>hehe</p>
-            </div>
+            <Layout>
+                <Card fluid>
+                    <Card.Content textAlign="center">
+                        <Card.Header>{this.props.loanTitle}</Card.Header>
+                        <Card.Meta>{this.props.loanContractAddress}</Card.Meta>
+                        <Card.Description>Lender: {this.props.lenderAddress}</Card.Description>
+                        <Card.Description>Borrower: {this.props.borrowerAddress}</Card.Description>
+                    </Card.Content>
+                </Card>
+                <Table>
+                    <Header>
+                        <Row>
+                            <HeaderCell>Month</HeaderCell>
+                            <HeaderCell>Date</HeaderCell>
+                            <HeaderCell>Balance</HeaderCell>
+                            <HeaderCell>Payment</HeaderCell>
+                            <HeaderCell>Interest</HeaderCell>
+                            <HeaderCell>Principal</HeaderCell>
+                            <HeaderCell>Paid</HeaderCell>
+                        </Row>
+                    </Header>
+                    <Body>{this.renderRows()}</Body>
+                </Table>
+            </Layout>
         );
     }
 }
