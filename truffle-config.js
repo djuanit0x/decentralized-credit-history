bf7d60a656dc232ff83a082886b46b762f6d6bfd
dennisjuanito <dennisjuanito@gmail.com>
2019-03-24 16:25:00 +0700

    t

--- truffle-config.js
+++ truffle-config.js
@@ -11,7 +11,7 @@ module.exports = {
         development: {
             host: '127.0.0.1',
             port: 8545,
-            network_id: '*' // Match any network id
+            network_id: '*' // To Match any network id
         },
         rinkeby: {
             provider: function() {
